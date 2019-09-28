8032ebf1986d2953817aca74e7997cfa2f935349
Elkin Mantilla <e.mantilla@uniandes.edu.co>
2019-09-28 17:57:22 -0500

    Ciclo 1 - Paso 2 - Se modifico codigo para que funcionara la validacion de una cadena con un numero de varios digitos (verde)

--- Estadistica.py
+++ Estadistica.py
@@ -3,6 +3,6 @@ _author_ = 'Elkin Mantilla, Juan Camilo Cardenas'
 class Estadistica:
     def calcularEstadistica (cadena):
         if cadena == "":
-            return 0;
+            return 0
         else:
-            return len(cadena)
+            return 1
