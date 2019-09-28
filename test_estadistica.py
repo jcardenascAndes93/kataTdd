a857836460e579d5d605c305582cb619ffb7aba8
Elkin Mantilla <e.mantilla@uniandes.edu.co>
2019-09-28 17:45:24 -0500

    Ciclo 1 - Paso 2 - Se agrega test para calcular numeros de elementos de una cadena con un numero (Rojo)

--- test_estadistica.py
+++ test_estadistica.py
@@ -2,5 +2,8 @@ from unittest import TestCase
 from Estadistica import Estadistica
 
 class EstadisticaTest(TestCase):
-    def test_calcularEstadistica(self):
+    def test_calcularElementosCadenaVacia(self):
         self.assertEqual(Estadistica.calcularEstadistica(""),0,"Cadena vacia")
+
+    def test_calcularElementosCadenaConUnNumero(self):
+        self.assertEqual(Estadistica.calcularEstadistica("1"), 1, "Un numero")
