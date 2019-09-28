5de9fd954ef91e25a8ced1ea78cb6c805cb51a1d
Elkin Mantilla <e.mantilla@uniandes.edu.co>
2019-09-28 18:08:48 -0500

    Ciclo 1 - Paso 5 se modifica codigo para que retorne un array con el numero de elementos (verde)

--- Estadistica.py
+++ Estadistica.py
@@ -3,9 +3,9 @@ _author_ = 'Elkin Mantilla, Juan Camilo Cardenas'
 class Estadistica:
     def calcularEstadistica (cadena):
         if cadena == "":
-            return 0
+            return [0]
         elif "," in cadena:
             numeros = cadena.split(",")
-            return len(numeros)
+            return [len(numeros)]
         else:
-            return 1
+            return [1]
