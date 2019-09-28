44e5a0403fc3de9fd53b0a42005e48c528922edd
Camilo Cardenas <j.cardenasc@uniandes.edu.co>
2019-09-28 18:36:17 -0500

    Ciclo 2 Paso 3- Se actualiza el codigo para calcular la cantidad de elementos y el minimo de una cadena con dos numero (verde)

--- Estadistica.py
+++ Estadistica.py
@@ -7,6 +7,6 @@ class Estadistica:
             return [0, 0]
         elif "," in cadena:
             numeros = cadena.split(",")
-            return [len(numeros)]
+            return [len(numeros), int(min(numeros))]
         else:
             return [1, int(cadena)]
