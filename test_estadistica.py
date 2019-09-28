0abd8c26e314bee7f978738d2db662852d673dc8
Elkin Mantilla <e.mantilla@uniandes.edu.co>
2019-09-28 18:08:20 -0500

    Ciclo 1 - Paso 5 se modifica test para que reciba un array con el numero de elementos (rojo)

--- test_estadistica.py
+++ test_estadistica.py
@@ -3,17 +3,17 @@ from Estadistica import Estadistica
 
 class EstadisticaTest(TestCase):
     def test_calcularElementosCadenaVacia(self):
-        self.assertEqual(Estadistica.calcularEstadistica(""),0,"Cadena vacia")
+        self.assertEqual(Estadistica.calcularEstadistica(""),[0],"Cadena vacia")
 
     def test_calcularElementosCadenaConUnNumero(self):
-        self.assertEqual(Estadistica.calcularEstadistica("1"), 1, "Un numero")
-        self.assertEqual(Estadistica.calcularEstadistica("2"), 1, "Un numero")
-        self.assertEqual(Estadistica.calcularEstadistica("10"), 1, "Un numero")
+        self.assertEqual(Estadistica.calcularEstadistica("1"), [1], "Un numero")
+        self.assertEqual(Estadistica.calcularEstadistica("2"), [1], "Un numero")
+        self.assertEqual(Estadistica.calcularEstadistica("10"), [1], "Un numero")
 
     def test_calcularElementosCadenaConDosNumero(self):
-        self.assertEqual(Estadistica.calcularEstadistica("1,2"), 2, "Dos numero")
+        self.assertEqual(Estadistica.calcularEstadistica("1,2"), [2], "Dos numero")
 
     def test_calcularElementosCadenaConNNumero(self):
-        self.assertEqual(Estadistica.calcularEstadistica("1,2,4,5"), 4, "N numero")
+        self.assertEqual(Estadistica.calcularEstadistica("1,2,4,5"), [4], "N numero")
 
 
