72b6870feeb504efeeb2d8d1dfb5a51feaa45fea
Camilo Cardenas <j.cardenasc@uniandes.edu.co>
2019-09-28 18:18:57 -0500

    Ciclo 2- Paso1 Se modifica el codigo para pasar el test de cantidad de elementos y minimo de una cadena vacia (verde)

--- Estadistica.py
+++ Estadistica.py
@@ -4,7 +4,7 @@ _author_ = 'Elkin Mantilla, Juan Camilo Cardenas'
 class Estadistica:
     def calcularEstadistica(cadena):
         if cadena == "":
-            return [0]
+            return [0, 0]
         elif "," in cadena:
             numeros = cadena.split(",")
             return [len(numeros)]
--- test_estadistica.py
+++ test_estadistica.py
@@ -2,27 +2,8 @@ from unittest import TestCase
 from Estadistica import Estadistica
 
 
-class EstadisticaTest(TestCase):
-    def test_calcularElementosCadenaVacia(self):
-        self.assertEqual(Estadistica.calcularEstadistica(""),
-                         [0], "Cadena vacia")
+class EstadisticaTest(TestCase):    
 
-    def test_calcularElementosCadenaConUnNumero(self):
-        self.assertEqual(Estadistica.calcularEstadistica("1"),
-                         [1], "Un numero")
-        self.assertEqual(Estadistica.calcularEstadistica("2"),
-                         [1], "Un numero")
-        self.assertEqual(Estadistica.calcularEstadistica(
-            "10"), [1], "Un numero")
-
-    def test_calcularElementosCadenaConDosNumero(self):
-        self.assertEqual(Estadistica.calcularEstadistica(
-            "1,2"), [2], "Dos numero")
-
-    def test_calcularElementosCadenaConNNumero(self):
-        self.assertEqual(Estadistica.calcularEstadistica(
-            "1,2,4,5"), [4], "N numero")
-    
     def test_calcularElementosYMinimoCadenaVacia(self):
         self.assertEqual(Estadistica.calcularEstadistica(""),
                          [0, 0], "Cadena vacia")
