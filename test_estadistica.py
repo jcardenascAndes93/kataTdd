62f567bb57ca0d825c9373039558e20ef15be839
Camilo Cardenas <j.cardenasc@uniandes.edu.co>
2019-09-28 18:30:24 -0500

    Ciclo 2 Paso 2- Se agrega test para calcular los elementos y el minimo de una cadena con un numero (rojo)

--- test_estadistica.py
+++ test_estadistica.py
@@ -2,8 +2,12 @@ from unittest import TestCase
 from Estadistica import Estadistica
 
 
-class EstadisticaTest(TestCase):    
+class EstadisticaTest(TestCase):
 
     def test_calcularElementosYMinimoCadenaVacia(self):
         self.assertEqual(Estadistica.calcularEstadistica(""),
                          [0, 0], "Cadena vacia")
+
+    def test_calcularElementosYMinimoUnNumero(self):
+        self.assertEqual(Estadistica.calcularEstadistica("3"),
+                         [1, 3], "Un numero")
