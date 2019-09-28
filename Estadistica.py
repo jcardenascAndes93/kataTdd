04a9cbef98a108d4e3dd9031041e05241d378802
Elkin Mantilla <e.mantilla@uniandes.edu.co>
2019-09-28 18:01:44 -0500

    Ciclo 1 - Paso 3 - Se modifica codigo para validacion de una cadena de dos numeros (verde)

--- Estadistica.py
+++ Estadistica.py
@@ -4,5 +4,8 @@ class Estadistica:
     def calcularEstadistica (cadena):
         if cadena == "":
             return 0
+        elif "," in cadena:
+            numeros = cadena.split(",")
+            return len(numeros)
         else:
             return 1
