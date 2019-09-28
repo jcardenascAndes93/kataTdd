c3e112ed48e832ef772ce32a060417430265f570
Elkin Mantilla <e.mantilla@uniandes.edu.co>
2019-09-28 18:54:54 -0500

    Ciclo 3 - Paso 1 - Se agrega test para validar el maximo en una cadena vacia (rojo)

--- test_estadistica.py
+++ test_estadistica.py
@@ -4,20 +4,6 @@ from Estadistica import Estadistica
 
 class EstadisticaTest(TestCase):
 
-    def test_calcularElementosYMinimoCadenaVacia(self):
+    def test_calcularElemMinMaxCadenaVacia(self):
         self.assertEqual(Estadistica.calcularEstadistica(""),
-                         [0, 0], "Cadena vacia")
-
-    def test_calcularElementosYMinimoUnNumero(self):
-        self.assertEqual(Estadistica.calcularEstadistica("3"),
-                         [1, 3], "Un numero")
-
-    def test_calcularElementosYMinimoDosNumeros(self):
-        self.assertEqual(Estadistica.calcularEstadistica("3, 1"),
-                         [2, 1], "Un numero")
-        self.assertEqual(Estadistica.calcularEstadistica("3, 11"),
-                         [2, 3], "Un numero")
-
-    def test_calcularElementosYMinimoNNumeros(self):
-        self.assertEqual(
-            Estadistica.calcularEstadistica("2, 4, 1, 10"), [4, 1])
+                         [0, 0, 0], "Cadena vacia")
