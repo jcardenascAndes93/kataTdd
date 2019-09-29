201b18198d0282f7bbd402dd59e89d11bfe79c60
Camilo Cardenas <j.cardenasc@uniandes.edu.co>
2019-09-28 19:11:56 -0500

    Ciclo 4 Paso 1- Se agrega test para agregar el calculo del promedio de una cadena vacia (rojo)

--- test_estadistica.py
+++ test_estadistica.py
@@ -6,16 +6,4 @@ class EstadisticaTest(TestCase):
 
     def test_calcularElemMinMaxCadenaVacia(self):
         self.assertEqual(Estadistica.calcularEstadistica(""),
-                         [0, 0, 0], "Cadena vacia")
-
-    def test_calcularElemMinMaxCadenaConUnNumero(self):
-        self.assertEqual(Estadistica.calcularEstadistica("3"),
-                         [1, 3, 3], "Un numero")
-
-    def test_calcularElemMinMaxCadenaConDosNumero(self):
-        self.assertEqual(Estadistica.calcularEstadistica("3,2"),
-                         [2, 2, 3], "Dos numeros")
-
-    def test_calcularElemMinMaxCadenaConNNumero(self):
-        self.assertEqual(Estadistica.calcularEstadistica("3,2,4,8,10"),
-                         [5, 2, 10], "N numeros")
+                         [0, 0, 0, 0], "Cadena vacia")
