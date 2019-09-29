10d3f4931ea0d8661e415d91c709df0989b302ba
Camilo Cardenas <j.cardenasc@uniandes.edu.co>
2019-09-28 19:15:59 -0500

    Ciclo 4 Paso 1- Se agrega test que agrega el promedio de una cadena con un numero (rojo)

--- test_estadistica.py
+++ test_estadistica.py
@@ -4,6 +4,10 @@ from Estadistica import Estadistica
 
 class EstadisticaTest(TestCase):
 
-    def test_calcularElemMinMaxCadenaVacia(self):
+    def test_calcularElemMinMaxPromCadenaVacia(self):
         self.assertEqual(Estadistica.calcularEstadistica(""),
                          [0, 0, 0, 0], "Cadena vacia")
+
+    def test_calcularElemMinMaxPromUnNumero(self):
+        self.assertEqual(Estadistica.calcularEstadistica("8"),
+                         [1, 8, 8, 8], "Cadena vacia")
