e840a67684e5a44fead804868c4dcb3c01ddd6b1
Camilo Cardenas <j.cardenasc@uniandes.edu.co>
2019-09-28 19:21:31 -0500

    Ciclo 4 Paso 3 - Se actualiza el codigo para calcular el promedio de una cadena con dos numeros (verde)

--- Estadistica.py
+++ Estadistica.py
@@ -9,11 +9,14 @@ class Estadistica:
             numeros = cadena.split(",")
             minimo = int(numeros[0])
             maximo = int(numeros[0])
+            suma = 0
             for n in numeros:
                 if int(n) > maximo:
                     maximo = int(n)
                 if minimo > int(n):
                     minimo = int(n)
-            return [len(numeros), minimo, maximo]
+                suma += int(n)
+            promedio = suma/len(numeros)
+            return [len(numeros), minimo, maximo, promedio]
         else:
             return [1, int(cadena), int(cadena), int(cadena)]
