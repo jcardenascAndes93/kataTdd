06261b0df2198c0fa6568f4c58a8ef68525af9d2
Elkin Mantilla <e.mantilla@uniandes.edu.co>
2019-09-28 19:03:40 -0500

    Ciclo 3 - Paso 3 - Se agrega test para validar maximo de una cadena de dos numeros (rojo)

--- test_estadistica.py
+++ test_estadistica.py
@@ -9,5 +9,9 @@ class EstadisticaTest(TestCase):
                          [0, 0, 0], "Cadena vacia")
 
     def test_calcularElemMinMaxCadenaConUnNumero(self):
-        self.assertEqual(Estadistica.calcularEstadistica("1"),
-                         [1, 1, 1], "Cadena vacia")
+        self.assertEqual(Estadistica.calcularEstadistica("3"),
+                         [1, 3, 3], "Un numero")
+
+    def test_calcularElemMinMaxCadenaConDosNumero(self):
+        self.assertEqual(Estadistica.calcularEstadistica("3,2"),
+                         [1, 2, 3], "Dos numeros")
